920568ba6941eec2b05975e00d82f77286d8a785
Ayush <ayushedith@gmail.com>
2024-03-08 12:40:41 +0530

    fixed some bugs

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -1,61 +1,33 @@
-import React, { useState } from 'react';
+import React, { useState } from 'react'
 import { useTodo } from '../contexts/TodoContext';
 
-function TodoItem({ todo }) {
-  const [isTodoEditable, setIsTodoEditable] = useState(false);
-  const [todoMsg, setTodoMsg] = useState(todo.todo);
-  const { updateTodo, deleteTodo, toggleComplete } = useTodo();
+function TodoForm() {
+    const [todo, setTodo] = useState("")
+    const {addTodo} = useTodo()
 
-  const editTodo = () => {
-    updateTodo(todo.id, { ...todo, todo: todoMsg });
-    setIsTodoEditable(false);
-  };
+    const add = (e) => {
+      e.preventDefault()
 
-  const toggleCompleted = () => {
-    if (!todo.completed) {
-      toggleComplete(todo.id);
+      if (!todo) return
+
+      addTodo({ todo, completed: false})
+      setTodo("")
     }
-  };
 
   return (
-    <div className={`flex border rounded-lg px-3 py-1.5 gap-x-3 shadow-md duration-300  text-black ${todo.completed ? "bg-[#c6e9a7]" : "bg-[#ccbed7]"}`}>
-      <input
-        type="checkbox"
-        className="cursor-pointer"
-        checked={todo.completed}
-        onChange={toggleCompleted}
-      />
-      <input
-        type="text"
-        className={`border outline-none w-full bg-transparent rounded-lg ${isTodoEditable ? "border-gray-500 px-2" : "border-transparent"} ${todo.completed ? "line-through" : ""}`}
-        value={todoMsg}
-        onChange={(e) => setTodoMsg(e.target.value)}
-        readOnly={!isTodoEditable}
-      />
-      {/* Edit, Save Button */}
-      <button
-        className={`inline-flex w-8 h-8 rounded-lg text-sm border justify-center items-center bg-gray-300 hover:bg-gray-400 shrink-0 ${todo.completed ? "opacity-50 cursor-not-allowed" : ""}`}
-        onClick={() => {
-          if (isTodoEditable) {
-            editTodo();
-          } else {
-            setIsTodoEditable(true);
-          }
-        }}
-        disabled={todo.completed}
-      >
-        {isTodoEditable ? "📁" : "✏️"}
-      </button>
-      {/* Delete Todo Button */}
-      <button
-        className={`inline-flex w-8 h-8 rounded-lg text-sm border justify-center items-center bg-gray-300 hover:bg-gray-400 shrink-0 ${todo.completed ? "opacity-50 cursor-not-allowed" : ""}`}
-        onClick={() => deleteTodo(todo.id)}
-        disabled={todo.completed}
-      >
-        ❌
-      </button>
-    </div>
+      <form onSubmit={add}  className="flex">
+          <input
+              type="text"
+              placeholder="Write Todo..."
+              className="w-full border border-black/10 rounded-l-lg px-3 outline-none duration-150 bg-white/20 py-1.5"
+              value={todo}
+              onChange={(e) => setTodo(e.target.value)}
+          />
+          <button type="submit" className="rounded-r-lg px-3 py-1 bg-green-600 text-white shrink-0">
+              Add
+          </button>
+      </form>
   );
 }
 
-export default TodoItem;
+export default TodoForm;
